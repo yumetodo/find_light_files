0df78d73ea213ce016a621d643527068ff967ba1
yumetodo <yume-wikijp@live.jp>
2023-07-25 21:03:11 +0900

    feat: コマンドライン解析の実装

---
+++ find_light_files/cmd_line.cpp
@@ -0,0 +1,39 @@
+﻿#include "cmd_line.hpp"
+#include <string_view>
+#include <iostream>
+#include <cstdlib>
+constexpr std::uint64_t default_lines_threshold = 12;
+constexpr const char* help_text = R"(find_light_files [options]
+OPTIONS
+--ext [string] target extension
+--path [path] search target path
+--threshold [integer] line count threshold to list up(only fewer files)
+)";
+command_options parse_options(int argc, char** argv)
+{
+    using namespace std::string_view_literals;
+    if (argc == 2 && argv[1] == "--help"sv) {
+        std::cerr << help_text << std::flush;
+        std::quick_exit(0);
+    }
+    command_options ret{};
+    ret.lines_threshold = default_lines_threshold;
+
+    for (int i = 0; i < argc; ++i) {
+        if (argv[i] == "--ext"sv && i + 2 < argc) {
+            ret.ext = argv[i + 1];
+            ++i;
+        } else if (argv[i] == "--path"sv && i + 2 < argc) {
+            ret.path = argv[i + 1];
+            ++i;
+        } else if (argv[i] == "--threshold"sv && i + 2 < argc) {
+            ret.lines_threshold = std::stoull(argv[i]);
+            ++i;
+        }
+    }
+    if (ret.ext.empty() || ret.path.empty()) {
+        std::cerr << help_text << std::flush;
+        std::quick_exit(1);
+    }
+    return ret;
+}
---
+++ find_light_files/cmd_line.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <string>
+#include <cstdint>
+struct command_options {
+    std::string ext;
+    std::string path;
+    std::uint64_t lines_threshold;
+};
+command_options parse_options(int argc, char** argv);
